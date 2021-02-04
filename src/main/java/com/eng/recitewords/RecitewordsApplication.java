0534734d49e8a24e5f89061b6371d4af949638f1
blackcat <13918461061@163.com>
2021-02-04 16:36:30 +0800

    更新文件上传路径

--- src/main/java/com/eng/recitewords/RecitewordsApplication.java
+++ src/main/java/com/eng/recitewords/RecitewordsApplication.java
@@ -1,13 +1,18 @@
 package com.eng.recitewords;
 
+import com.eng.recitewords.config.FileStorageConfig;
 import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @MapperScan("com.eng.recitewords.mapper")
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageConfig.class
+})
 public class RecitewordsApplication {
 
     public static void main(String[] args) {
